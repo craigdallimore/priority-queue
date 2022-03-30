c78cd36336b4bc1bfc5682f51782891e71aabf12
craigdallimore <decoy9697@gmail.com>
2022-03-30 22:16:46 +1300

    Further upgrades and conversion to ts.
    Tests now run and pass

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/* eslint @typescript-eslint/no-var-requires: "off" */
+
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+};
